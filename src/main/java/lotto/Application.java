0299aaaea976ca3c152b2cac0258498bcf37bc08
Jimin <gift040404@naver.com>
2025-11-03 02:39:02 +0900

    refactor: Initialize LottoController in Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import controller.LottoController;
+import domain.Lotto;
+import domain.Lottos;
+import model.ConcreteLottoGenerator;
+import model.ConcreteRandomNumberExtractor;
+import model.LottoGenerator;
+import model.LottoMatcher;
+import utils.InputHandler;
+import utils.InputParser;
+import utils.LottoValidator;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        try {
+            LottoController lottoController = new LottoController(
+                    new InputHandler(new InputParser()),
+                    new InputView(),
+                    new OutputView(),
+                    new ConcreteLottoGenerator(new Lottos(), new ConcreteRandomNumberExtractor()),
+                    new LottoMatcher());
+
+            lottoController.run();
+
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+        }
     }
 }
