f9c18e6877ea654aaa063b30efc5f8e060aff672
Jimin <gift040404@naver.com>
2025-11-03 02:27:05 +0900

    feat: Add Lottos Class

---
+++ src/main/java/domain/Lottos.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public void addLotto(Lotto lotto) {
+        lottos.add(lotto);
+    }
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+}
