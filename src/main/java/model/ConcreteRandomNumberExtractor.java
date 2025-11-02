99916762e4451893688387e7bd13e24fed045299
Jimin <gift040404@naver.com>
2025-11-03 02:26:41 +0900

    feat: Add RandomNumberExtractor
    Add RandomNumberExtractor interface and ConcreteRandomNumberExtractor implementation

---
+++ src/main/java/model/ConcreteRandomNumberExtractor.java
@@ -0,0 +1,12 @@
+package model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import dto.LottoConstants;
+import java.util.List;
+
+public class ConcreteRandomNumberExtractor implements RandomNumberExtractor {
+    @Override
+    public List<Integer> getRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LottoConstants.MIN_NUMBER,LottoConstants.MAX_NUMBER,LottoConstants.LOTTO_SIZE);
+    }
+}
