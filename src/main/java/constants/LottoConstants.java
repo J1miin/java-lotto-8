d8535427087f3ca3ded16ec0195d43fdac59feeb
Jimin <gift040404@naver.com>
2025-11-03 16:09:35 +0900

    refactor: Change package and add final
    - move LottoConstants to constants package
    - make LOTTO_PRICE_UNIT final

--- src/main/java/constants/LottoConstants.java
+++ src/main/java/constants/LottoConstants.java
@@ -1,7 +1,7 @@
-package dto;
+package constants;
 
 public class LottoConstants {
-    public static int LOTTO_PRICE_UNIT = 1000;
+    public static final int LOTTO_PRICE_UNIT = 1000;
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
